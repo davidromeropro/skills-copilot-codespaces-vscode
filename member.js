3b8209ad8ca80938d0322618b12905b0ca8b976e
davidromeropro <davidromeropro@gmail.com>
2023-07-18 20:04:31 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+	return {
+		name: 'skillsMember',
+		templateUrl: 'app/components/member/member.html',
+		controller: 'MemberController',
+		controllerAs: 'vm',
+		bindToController: true
+	}
+}
